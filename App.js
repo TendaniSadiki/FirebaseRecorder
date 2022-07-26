ad880ec4424956e975d8ad8ef321b91339370b72
TendaniSadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-07-26 02:30:15 +0200

    {pages added }

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import AuthRoutes from './src/Auth/routes';
 
-export default function App() {
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from "./screens/Login"
+import HomeScreen from "./screens/Home"
+import SignUpScreen from "./screens/SignUp"
+import firebase from 'firebase/app';
+import "firebase/auth";
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const firebaseConfig = {
+    ...
+  };
+
+  //Checking if firebase has been initialized
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  } else {
+    firebase.app();
+  }
+
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user != null) {
+      setIsLoggedIn(true)
+    } else {
+      setIsLoggedIn(false);
+    }
+  });
+
+
+
   return (
-    <View style={styles.container}>
-     <AuthRoutes/> 
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {isLoggedIn ? <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+      </Stack.Navigator> :
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Sign Up" component={SignUpScreen} options={{ headerShown: false }} />
+        </Stack.Navigator>}
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
